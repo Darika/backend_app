b927d075ff225a8fd3ec2978f8137050b2df56ac
Darika <darikapiterski@gmail.com>
2022-04-25 11:55:29 +0300

    first

---
+++ database/migrations/2022_04_21_101541_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('user_id');
+            $table->integer('type_id');
+            $table->dateTime('date_start');
+            $table->dateTime('date_end');
+            $table->integer('priority_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
